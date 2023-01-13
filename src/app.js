3d6e468295a0139ffc5e5e0de4a50c355ac19e44
hugo <hugordura@hotmail.com>
2023-01-13 18:54:52 -0400

    criacao Crud

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import  express  from "express";
 
 const app = express();
+
+app.use(express.json());//habilita interpretador de json
+
 const livros =[
     {id:1,"titulo":"Senhor doas aneis"},
     {id:2,"titulo":"O Hobit"}
@@ -10,9 +13,42 @@ app.get('/',(req,resp)=>{
     resp.status(200).send('Curso de node')
 })
 
+//traz a list de livros
 app.get('/livros',(req,resp)=>{
     resp.status(200).json(livros)
 })
+
+
+app.get('/livros/:id',(req,resp)=>{
+    let index = buscaLivro(req.params.id);
+    resp.json(livros[index]);
+})
+
+//adicionar livros
+app.post('/livros',(req,resp)=>{
+    livros.push(req.body);
+    resp.status(201).send('adicionado com sucesso')
+})
+
+app.put('/livros/:id',(req,resp)=>{
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo =req.body.titulo;
+    
+    resp.json(livros);
+})
+
+app.delete('/livros/:id',(req,resp)=>{
+    let {id}= req.params //desiturturacao eh igual req.params.id
+    let index = buscaLivro(id);
+    livros.splice(index,1);
+    
+    resp.send(`Livro ${id} removido com sucesso`);
+})
+
+
+function buscaLivro(id){
+    return livros.findIndex(livro =>livro.id == id)
+}
 
 //exportar para o server e escutar
 
