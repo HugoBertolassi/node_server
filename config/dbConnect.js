047e43ac2a359ccb45793e383bfbe0930c41959b
hugo <hugordura@hotmail.com>
2023-01-15 16:09:48 -0400

    password

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,9 @@
+import { mongodbPassword } from "../passwords.js";
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://BERTOLASSI:RFh6kw1yGxeTu7Uq@node.ql25wui.mongodb.net/NODE");
-//mongoose.connect("mongodb+srv://BERTOLASSI:RFh6kw1yGxeTu7Uq@node.ql25wui.mongodb.net/?retryWrites=true&w=majority")
+
+mongoose.connect(`mongodb+srv://BERTOLASSI:${mongodbPassword}@node.ql25wui.mongodb.net/NODE`);
+//mongoose.connect("mongodb+srv://BERTOLASSI:<password>@node.ql25wui.mongodb.net/?retryWrites=true&w=majority") // ?retryWrites=true&w=majority= nome projeto
 
 let db =mongoose.connection;
 
